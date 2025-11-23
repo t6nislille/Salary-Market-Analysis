bf24032b0bdb169276dd97b335013356ef9b9cd2
Tõnis <tonislille@gmail.com>
2025-11-23 15:07:38 +0200

    Add JSON body

--- src/pages/api/average_salary.ts
+++ src/pages/api/average_salary.ts
@@ -3,6 +3,28 @@ import type { NextResponse } from "next/server";
 export async function POST (req: Request) {
     const { fieldValue } = await req.json();
     const body = {
-
+  "query": [
+    {
+      "code": "Näitaja",
+      "selection": {
+        "filter": "item",
+        "values": [
+          "GR_W_AVG"
+        ]
+      }
+    },
+    {
+      "code": "Tegevusala",
+      "selection": {
+        "filter": "item",
+        "values": [
+          fieldValue
+        ]
+      }
     }
+  ],
+  "response": {
+    "format": "json-stat2"
+  }
+};
 }
