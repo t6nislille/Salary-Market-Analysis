e19791c61e42904b67f210bbc33b8fd804973998
Tõnis <tonislille@gmail.com>
2025-11-24 13:03:10 +0200

    Import dependencies and create fiendDropdown function

--- src/components/dropdown.tsx
+++ src/components/dropdown.tsx
@@ -1,4 +1,9 @@
-'use client'
-import { useState
+import type { Selection } from "@heroui/react";
+import React from "react";
+import {Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Button} from "@heroui/react";
+
+export default function fieldDropdown() {
+    const [selectedKeys, setSelectedKeys] = React.useState<Selection>(new Set(["text"]));
+
     
- } from "react"
+}
