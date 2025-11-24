66d15f00bd7433709ed6ef52fdbd5d43ec85d0c9
Tõnis <tonislille@gmail.com>
2025-11-24 23:04:26 +0200

    Fix POST request

--- src/app/api/dropdown_fields/route.ts
+++ src/app/api/dropdown_fields/route.ts
@@ -1,13 +1,12 @@
-import { error } from "console";
 import { NextResponse } from "next/server";
 
-export async function GET() {
+export async function POST(req: Request) {
     try {
-
+        
         const body = {
     "query": [
         {"code": "Näitaja", "selection": { "filter": "item", "values": [ "GR_W_AVG" ] } },
-        { "code": "Tegevusala", "selection": { "filter": "item", "values": [ ] } } 
+        { "code": "Tegevusala", "selection": { "filter": "item", "values": ["*"] } } 
     ],
         "response": { "format": "json-stat2" }
     };
@@ -15,11 +14,11 @@ export async function GET() {
     // Making a request
     const URL = process.env.STAT_API_URL;
     const res = await fetch(URL!, {
-    method: "POST",
-    body: JSON.stringify(body),
-    headers: {"Content-Type": "application/json"},
+        method: "POST",
+        body: JSON.stringify(body),
+        headers: {"Content-Type": "application/json"},
     });
-
+    
     // Read JSON
     const data = await res.json();
 
