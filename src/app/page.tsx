f98ee19a4fd3f57b5f6780133ac30fd6768081f5
Tõnis <tonislille@gmail.com>
2025-11-24 14:32:58 +0200

    Hold average salary value and corresponding year with field

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,23 @@ import React, {useState} from "react";
 import FieldDropdown from "../components/dropdown";
 
 export default function Home() {
+  // Hold salary data
+  const [salaryData, setSalaryData] = useState<number[]>([]);
+  // Hold years
+  const [years, setYears] = useState<string[]>([]);
+  // Selected field
+  const [selectedName, setSelectedName] = useState<string>("Text");
+  
 
   return (
     <main className="flex flex-col items-center">
       <h1 className="text-3xl font-bold">Keskmine palk</h1>
       
-      <FieldDropdown />
+      <FieldDropdown  />
+      <div>
+        <h2>Valdkond: {selectedName}</h2>
+
+      </div>
 
     </main>
   );
