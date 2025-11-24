9e8be1e8178adf0e378469dff0c3f2de658ab0b5
Tõnis <tonislille@gmail.com>
2025-11-24 13:12:47 +0200

    Add dropdown menu

--- src/components/dropdown.tsx
+++ src/components/dropdown.tsx
@@ -21,6 +21,10 @@ return (
                 {selectedValue}
             </Button>
         </DropdownTrigger>
+        <DropdownMenu
+            >
+
+        </DropdownMenu>
     </Dropdown>
 );
 };
