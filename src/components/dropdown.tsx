8035aeda1bd3066d71a163130d10d118aa827e12
Tõnis <tonislille@gmail.com>
2025-11-24 13:07:48 +0200

    Convert Set to String

--- src/components/dropdown.tsx
+++ src/components/dropdown.tsx
@@ -2,8 +2,14 @@ import type { Selection } from "@heroui/react";
 import React from "react";
 import {Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Button} from "@heroui/react";
 
+// Hold Keys as Set
+// First value is "text"
 export default function fieldDropdown() {
     const [selectedKeys, setSelectedKeys] = React.useState<Selection>(new Set(["text"]));
 
-    
+    // Convert Set to String
+    const selectedValue = React.useMemo(
+        () => Array.from(selectedKeys).join(", "),
+        [selectedKeys],
+    )
 }
