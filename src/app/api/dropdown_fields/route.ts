0b27bf89d9d4aae695120e9c311b73432d53f9eb
Tõnis <tonislille@gmail.com>
2025-11-24 17:23:08 +0200

    Make a request to STAT API

--- src/app/api/dropdown_fields/route.ts
+++ src/app/api/dropdown_fields/route.ts
@@ -1,5 +1,29 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
+    try {
+
+        const body = {
+    "query": [
+        {"code": "Näitaja", "selection": { "filter": "item", "values": [ "GR_W_AVG" ] } },
+        { "code": "Tegevusala", "selection": { "filter": "item", "values": [ ] } } 
+    ],
+        "response": { "format": "json-stat2" }
+    };
+
+    // Making a request
+    const URL = process.env.STAT_API_URL;
+    const res = await fetch(URL!, {
+    method: "POST",
+    body: JSON.stringify(body),
+    headers: {"Content-Type": "application/json"},
+    });
+
+    // Read JSON
+    const data = await res.json();
+
+    // List for field names
     
+
+    }
 }
