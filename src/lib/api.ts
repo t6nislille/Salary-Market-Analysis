66c060bbacc496023901ef349ccfd277aaf6d435
Tõnis <tonislille@gmail.com>
2025-11-26 01:06:44 +0200

    Fetch from dropdown_fields

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,5 +2,9 @@ import {FieldOption} from "../lib/types";
 
 // Fetch dropdown fields
 export async function fetchDropdownFields() {
-    
+    const res = await fetch("/api/dropdown_fields", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({})
+    });
 }
