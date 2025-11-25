3e477db7f8834792341eaed48bd8c564817e557e
Tõnis <tonislille@gmail.com>
2025-11-25 16:21:08 +0200

    Make a request to OpenAI API

--- src/app/api/ai_summery/route.ts
+++ src/app/api/ai_summery/route.ts
@@ -3,12 +3,38 @@ import { NextResponse } from "next/server";
 
 // API Route
 export async function POST(req: Request) {
-    
+
     const apiKey = process.env.OPENAI_API_KEY;
     if (!apiKey) {
         return NextResponse.json(
             {error: "API KEY is not set"},
             { status: 500}
-        )
+        );
     }
+
+    const prompt = ""
+
+    // Make a request to OpenAI API
+    const openAiRes = await fetch("https://api.openai.com/v1/chat/completions", {
+        method: "POST",
+        headers: {
+            Authorization: "Bearer ${apiKey}",
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            model: "gpt-40",
+            messages: [
+                {
+                    role: "system",
+                    content: ""
+                },
+                {
+                    role: "user",
+                    content: prompt,
+                },
+            ],
+            // Lower temperature for more consistent results
+            temperature: 0.4, 
+        }),
+    });
 }
