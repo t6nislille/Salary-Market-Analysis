2ffa778c41996c660826caaffe1fe6025370c5ec
Tõnis <tonislille@gmail.com>
2025-11-25 16:12:58 +0200

    Import API KEY & added error message

--- src/app/api/ai_summery/route.ts
+++ src/app/api/ai_summery/route.ts
@@ -1,6 +1,14 @@
+import { error } from "console";
 import { NextResponse } from "next/server";
 
 // API Route
 export async function POST(req: Request) {
-
+    
+    const apiKey = process.env.OPENAI_API_KEY;
+    if (!apiKey) {
+        return NextResponse.json(
+            {error: "API KEY is not set"},
+            { status: 500}
+        )
+    }
 }
