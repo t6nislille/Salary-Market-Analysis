f1a287c742a44bf0af768e66834bb39724959012
Tõnis <tonislille@gmail.com>
2025-11-26 01:10:38 +0200

    Added error handeling

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,12 +1,15 @@
+import { error } from "console";
 import {FieldOption} from "../lib/types";
 
 // Fetch dropdown fields
-export async function fetchDropdownFields() {
+export async function fetchDropdownFields(): Promise<FieldOption[]> {
     const res = await fetch("/api/dropdown_fields", {
         method: "POST",
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify({})
     });
+
+    if (!res.ok) throw new Error("Failed to fetch dropdown fields!");
 
     const data = await res.json();
     return data.fields ?? [];
